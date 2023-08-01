8458bfd8bcb62435f271ce01e35a437c17bb136e
Victor Eke <evavic44@gmail.com>
2023-08-01 23:24:01 +0100

    final fix

--- app/(site)/components/global/Theme.tsx
+++ app/(site)/components/global/Theme.tsx
@@ -1,6 +1,5 @@
 "use client";
-
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import SunIcon from "../../icons/SunIcon";
 import MoonIcon from "../../icons/MoonIcon";
 
