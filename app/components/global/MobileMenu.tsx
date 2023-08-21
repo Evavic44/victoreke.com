8842e93bee0efe2daefaa56f8886dda37630acd3
Victor Eke <evavic44@gmail.com>
2023-08-21 02:33:00 +0100

    add mobile menu
    closes #38

---
+++ app/components/global/MobileMenu.tsx
@@ -0,0 +1,97 @@
+"use client";
+import Link from "next/link";
+import Image from "next/image";
+import { useState } from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import {
+  HiBeaker,
+  HiBookmarkAlt,
+  HiCamera,
+  HiOutlineX,
+  HiUser,
+} from "react-icons/hi";
+import Logo from "../../../public/logo.png";
+
+export default function MobileMenu() {
+  const [navShow, setNavShow] = useState(false);
+  const data = [
+    {
+      title: "About",
+      href: "/about",
+      icon: HiUser,
+    },
+    {
+      title: "Projects",
+      href: "/projects",
+      icon: HiBeaker,
+    },
+    {
+      title: "Blog",
+      href: "/blog",
+      icon: HiBookmarkAlt,
+    },
+    {
+      title: "Photos",
+      href: "/photos",
+      icon: HiCamera,
+    },
+  ];
+
+  const onToggleNav = () => {
+    setNavShow((status) => {
+      status
+        ? (document.body.style.overflow = `${status ? "auto" : "hidden"}`)
+        : null;
+      return !status;
+    });
+  };
+
+  return (
+    <>
+      <button
+        aria-label="Toggle Menu"
+        onClick={onToggleNav}
+        className="md:hidden dark:bg-primary-bg bg-secondary-bg border dark:border-zinc-800 border-zinc-200 rounded-md p-2"
+      >
+        <RxHamburgerMenu className="text-xl" />
+      </button>
+      <div
+        className={`md:hidden fixed left-0 top-0 z-10 h-full w-full transform duration-[600ms] ease-[cubic-bezier(0.7,0,0,1)] dark:bg-zinc-900 bg-white ${
+          navShow ? "translate-x-0 rounded-none" : "translate-x-full"
+        }`}
+      >
+        <div className="flex items-center justify-between mt-6 px-8">
+          <Link href="/" onClick={onToggleNav}>
+            <Image src={Logo} width={35} height={35} alt="logo" />
+          </Link>
+
+          <button
+            aria-label="Toggle Menu"
+            onClick={onToggleNav}
+            className={`md:hidden dark:bg-primary-bg bg-secondary-bg border dark:border-zinc-800 border-zinc-200 rounded-full p-2 duration-500 ${
+              !navShow ? "-rotate-[360deg]" : null
+            }`}
+          >
+            <HiOutlineX className="text-xl" />
+          </button>
+        </div>
+        <nav className="flex flex-col mt-6">
+          {data.map((link) => (
+            <Link
+              key={link.title}
+              href={link.href}
+              className="flex items-center gap-x-2 font-incognito font-semibold text-lg dark:shadow-line-dark shadow-line-light p-6 group"
+              onClick={onToggleNav}
+            >
+              <link.icon
+                className="text-zinc-500 group-hover:dark:text-white group-hover:text-zinc-800 duration-300"
+                aria-hidden="true"
+              />
+              {link.title}
+            </Link>
+          ))}
+        </nav>
+      </div>
+    </>
+  );
+}
